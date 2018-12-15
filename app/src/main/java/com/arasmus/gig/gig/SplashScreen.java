945abaec4c706d872d185a858521c77237931da9
Yashashavi <yash1995.momyan@gmail.com>
2018-12-15 20:47:11 +0530

    vectors-nav drawer

--- app/src/main/java/com/arasmus/gig/gig/SplashScreen.java
+++ app/src/main/java/com/arasmus/gig/gig/SplashScreen.java
@@ -2,6 +2,7 @@ package com.arasmus.gig.gig;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -9,5 +10,16 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE
+                        // Set the content to appear under the system bars so that the
+                        // content doesn't resize when the system bars hide and show.
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        // Hide the nav bar and status bar
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
     }
 }
